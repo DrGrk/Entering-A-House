6e662ff50117ef22c675563efee1f405743a6122
David Guichon <davidrguichon@gmail.com>
2017-03-15 16:19:17 -0400

    Initial Commit

---
+++ Entering A House/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Entering A House
+//
+//  Created by David Guichon on 2017-03-15.
+//  Copyright © 2017 David Guichon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Entering A House/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Entering A House
+//
+//  Created by David Guichon on 2017-03-15.
+//  Copyright © 2017 David Guichon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
